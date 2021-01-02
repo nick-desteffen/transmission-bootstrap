b3cf220dafbd565c621280c9e0ea2c6a85b91122
Nick DeSteffen <nick.desteffen@gmail.com>
2021-01-01 22:28:37 -0600

    Updates for Transmission 3.0 and systemd

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Transmission Bootstrap
 ==============
 
-Bash script to install Transmission BitTorrent client (v2.9.2) and have the daemon run on boot.  Tested on Ubuntu 16.04.1.
-  
-  
-To bootstrap Transmission:  
+Bash script to install Transmission BitTorrent client (v3.0.0) and have the daemon run on boot.  Tested on Ubuntu Server 20.04.1 LTS.
+
+
+To bootstrap Transmission:
 
 `curl -L https://raw.github.com/nick-desteffen/transmission-bootstrap/master/bootstrap_transmission.sh | sudo bash`
--- bootstrap_transmission.sh
+++ bootstrap_transmission.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-apt-get -y -m install build-essential libevent-dev intltool libssl-dev libcurl4-openssl-dev pkg-config
+apt-get -y -m install build-essential libevent-dev intltool libssl-dev libcurl4-openssl-dev pkg-config zlib1g-dev
 
 cd /tmp
-wget https://github.com/transmission/transmission-releases/raw/master/transmission-2.94.tar.xz
-tar -xvJf transmission-2.94.tar.xz
-cd transmission-2.94
+wget https://github.com/transmission/transmission-releases/raw/master/transmission-3.00.tar.xz
+tar -xvJf transmission-3.00.tar.xz
+cd transmission-3.00
 
 ./configure
 make
@@ -16,14 +16,13 @@ adduser --disabled-password --shell `which bash` --gecos "transmission,," transm
 touch /var/log/transmission.log
 chown transmission.transmission /var/log/transmission.log
 
-cd /etc/init.d
-wget https://raw.github.com/nick-desteffen/transmission-bootstrap/master/init.d/transmission-daemon
-chmod +x /etc/init.d/transmission-daemon
+cd /etc/systemd/system
 
-update-rc.d transmission-daemon defaults
+wget https://raw.github.com/nick-desteffen/transmission-bootstrap/master/systemd/transmission-daemon.service
 
-cd /home/transmission
-wget https://raw.github.com/nick-desteffen/transmission-bootstrap/master/auto-unrar.sh
+chmod 644 /etc/systemd/system/transmission-daemon.service
+systemctl start transmission-daemon
+systemctl enable transmission-daemon
 
 /usr/local/bin/transmission-daemon --version
 
